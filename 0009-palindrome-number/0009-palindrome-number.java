037bbaac1394a2612c1479cfd71959d95691defd
Shruti Jagtap <149305563+shrutijagtap1208@users.noreply.github.com>
2026-02-23 13:34:08 +0530

    Time: 4 ms (99.71%), Space: 46 MB (53.11%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+
+        while(x>0){
+            rev= rev*10 + x%10;
+            x= x/10;
+        }
+        if(temp==rev){
+            return true;
+
+        }
+        else{
+            return false;
+        }
+
+
+
+        
+    }
+
+    
+}
